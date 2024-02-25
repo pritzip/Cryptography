d964b90f563690a1b4f59d61230f6d470cf457cd
pritzip <82364879+pritzip@users.noreply.github.com>
2024-02-25 03:29:08 +0200

    Update double_ratchet.py

--- double_ratchet.py
+++ double_ratchet.py
@@ -1,18 +1,17 @@
 # requires to install cryptography library and latest python [pip install cryptography]
-
+#--------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------#
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-
-
+#--------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------#
 class SymmetricRatchet:
     def __init__(self, key):
         self.key = key
         self.counter = 0
-
-  
+#--------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------#
     def next(self):
         self.counter += 1
@@ -22,33 +21,30 @@ class SymmetricRatchet:
             salt=None,
             info=str(self.counter).encode(),
         ).derive(self.key)
-
 #--------------------------------------------------------------------------------------------------------#
-
+#--------------------------------------------------------------------------------------------------------#
 class DoubleRatchet:
     def __init__(self, shared_secret):
         self.root_ratchet = SymmetricRatchet(shared_secret)
         self.DHratchet = X25519PrivateKey.generate()
         self.refresh_ratchet()
-
-  
 #--------------------------------------------------------------------------------------------------------#
-  
+#--------------------------------------------------------------------------------------------------------#
     def refresh_ratchet(self):
-        dh_send = self.DHratchet.private_bytes()
-        shared_send = self.root_ratchet.next(dh_send)
+        dh_send = self.DHratchet.exchange(self.DHratchet.public_key())
+        shared_send = self.root_ratchet.next()
         self.send_ratchet = SymmetricRatchet(shared_send)
-
+#--------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------#
     def ratchet_and_encrypt(self, plaintext):
         self.refresh_ratchet()
         key = self.send_ratchet.next()
         return AESGCM(key).encrypt(nonce=b'\x00'*12, data=plaintext, associated_data=None)
-
+#--------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------#
     def decrypt(self, ciphertext, pbkey):
         dh_recv = X25519PrivateKey.from_public_bytes(pbkey).exchange(self.DHratchet.public_key())
-        shared_recv = self.root_ratchet.next(dh_recv)
+        shared_recv = self.root_ratchet.next()
         recv_ratchet = SymmetricRatchet(shared_recv)
         key = recv_ratchet.next()
         return AESGCM(key).decrypt(nonce=b'\x00'*12, data=ciphertext, associated_data=None)
